ad457bc6e120a5b3bf82511baade0f36379cbdf9
lucas.harmatiuk <lucasjharmatiuk@gmail.com>
2025-10-21 15:15:45 -0300

    refact: abstract CRUD

--- src/maddr_api/services/main.py
+++ src/maddr_api/services/main.py
@@ -1,4 +1,48 @@
 from enum import Enum
+from typing import Generic, TypeVar, Type, Optional, Any
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+
+ModelType = TypeVar("ModelType")
+CreateSchemaType = TypeVar("CreateSchemaType")
+
+
+class BaseCRUD(Generic[ModelType, CreateSchemaType]):
+    """
+    Generic CRUD operations for any model.
+    """
+
+    def __init__(self, model: Type[ModelType], session: Session):
+        self.model = model
+        self.session = session
+
+    def create(self, data: CreateSchemaType) -> ModelType:
+        """
+        Create a new record in the database.
+        """
+
+        obj_data = data.model_dump()
+        db_data = self.model(**obj_data)
+
+        self.session.add(db_data)
+        self.session.commit()
+        self.session.refresh(db_data)
+
+        return db_data
+
+    def read(self, search_field: str, value: Any) -> Optional[ModelType]:
+        """
+        Read a record from the database by a specific field.
+        """
+
+        record = self.session.scalar(
+            select(self.model).where(
+                getattr(self.model, search_field) == value
+            )
+        )
+
+        return record
 
 
 class AccountSearchField(str, Enum):
