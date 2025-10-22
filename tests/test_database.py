9db80f1ad9edb511d5ae806a34297e9bc7ef5f1b
lucas.harmatiuk <lucasjharmatiuk@gmail.com>
2025-10-22 18:51:18 -0300

    add

---
+++ tests/test_database.py
@@ -0,0 +1,29 @@
+from maddr_api.models.account import Account
+from sqlalchemy import select
+from dataclasses import asdict
+
+def test_create_account_in_database(session, mock_db_time):
+    with mock_db_time(model=Account) as time:
+        new_account = Account(
+            username="test_criando_account_in_db",
+            email="test_criando_account_in_db@gmail.com",
+            password="test_criando_account_in_db",
+        )
+
+        session.add(new_account)
+        session.commit()
+
+    account = session.scalar(
+        select(Account).where(Account.username == "test_criando_account_in_db")
+    )
+
+    data_expect = dict(
+        id=1,
+        username="test_criando_account_in_db",
+        password="test_criando_account_in_db",
+        email="test_criando_account_in_db@gmail.com",
+        created_at=time,
+        updated_at=time,
+    )
+
+    assert asdict(account) == data_expect
