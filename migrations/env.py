3d3eaf42aaa5d2e601c95fac16cf91ffcd0f1481
lucas.harmatiuk <lucasjharmatiuk@gmail.com>
2025-10-23 18:24:05 -0300

    add

--- migrations/env.py
+++ migrations/env.py
@@ -2,7 +2,6 @@ import asyncio
 from sqlalchemy.ext.asyncio import async_engine_from_config
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
@@ -56,14 +55,14 @@ def run_migrations_offline() -> None:
         context.run_migrations()
 
 
-def do_run_migrations(connection): 
+def do_run_migrations(connection):
     context.configure(connection=connection, target_metadata=target_metadata)
 
     with context.begin_transaction():
         context.run_migrations()
 
 
-async def run_async_migrations(): 
+async def run_async_migrations():
     connectable = async_engine_from_config(
         config.get_section(config.config_ini_section),
         prefix="sqlalchemy.",
@@ -76,7 +75,7 @@ async def run_async_migrations():
     await connectable.dispose()
 
 
-def run_migrations_online(): 
+def run_migrations_online():
     asyncio.run(run_async_migrations())
 
 
