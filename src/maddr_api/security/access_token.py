8b8b2a109c040e82c8695afc0801bc0e3ff604d2
lucas.harmatiuk <lucasjharmatiuk@gmail.com>
2025-10-24 21:40:51 -0300

    ajuste

--- src/maddr_api/security/access_token.py
+++ src/maddr_api/security/access_token.py
@@ -3,16 +3,16 @@ from zoneinfo import ZoneInfo
 
 from jwt import encode
 
-SECRET_KEY = "your_secret_key"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+from src.maddr_api.config.settings import Settings
+
+settings = Settings()
 
 
 def create_access_token(data: dict) -> str:
     to_encode = data.copy()
     expire = datetime.now(tz=ZoneInfo("UTC")) + timedelta(
-        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
     )
     to_encode.update({"exp": expire})
-    encoded_jwt = encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
--- tests/test_security.py
+++ tests/test_security.py
@@ -1,18 +1,18 @@
 from jwt import decode
 
 from maddr_api.security.access_token import (
-    SECRET_KEY,
-    ALGORITHM,
     create_access_token,
 )
+from maddr_api.config.settings import Settings
 
+settings = Settings()
 
 def test_create_access_token():
     data = dict(test="test")
 
     token = create_access_token(data)
 
-    decoded_data = decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    decoded_data = decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
 
     assert decoded_data["test"] == "test"
     assert "exp" in decoded_data
