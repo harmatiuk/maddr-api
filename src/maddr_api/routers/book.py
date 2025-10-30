a656cd04a22f69478ac1a468874fb508ccbce56a
lucas.harmatiuk <lucasjharmatiuk@gmail.com>
2025-10-30 17:37:57 -0300

    add

--- src/maddr_api/routers/book.py
+++ src/maddr_api/routers/book.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter, Depends
 from maddr_api.config.database import DatabaseSession
 from http import HTTPStatus
+from maddr_api.models.account import Account
 from maddr_api.schemas.book import (
     BookCreate,
     BookPublic,
 )
+from maddr_api.security.get_current_user import get_current_user
 from maddr_api.services.book import BookService
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import Annotated
@@ -24,5 +26,6 @@ router = APIRouter(prefix="/book", tags=["book"])
 async def create_book(
     book_data: BookCreate,
     session: DatabaseSession = Depends(DatabaseSession.get_session),
+    current_user: Account = Depends(get_current_user),
 ) -> BookPublic:
     return await BookService(session).create_book(book_data)
